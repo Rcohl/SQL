d4da5f38d052a4522cab22e84afb2da6328acf8a
Rcohl <corentin.orczyk@outlook.fr>
2023-07-05 15:09:43 +0200

    exercice 2 SQL

--- exercice-02.sql
+++ exercice-02.sql
@@ -25,51 +25,68 @@ WHERE id = 6
 -- Insérez un tag :
 -- - nom : SASS
 -- - description : aucune
-
+INSERT INTO tag (id, name, description)
+VALUES (NULL, 'SASS', NULL);
 -- Exo 2.2
 -- Insérez une school year :
 -- - nom: Promo 2023
 -- - description : La promo de l'année 2023
 -- - date de début : 15 mars 2023
 -- - date de fin : aucune
-
--- Exo 2.3
+INSERT INTO school_year (id, name, description, start_date, end_date)
+VALUES (NULL, 'Promo 2023', "La promo de l\'année 2023", '2023-03-15', NULL)
+-- Exo 2.3'
 -- Créez une relation entre un student et un tag :
 -- - student : Toto (id 1)
 -- - tag : SASS (id inconnu)
 -- Note : c'est à vous de retrouver l'id du tag mais vous pouvez l'inscrire en dur dans la requête.
-
+SELECT id FROM tag WHERE name = 'SASS'; -- pour retrouver l'id de SASS
+INSERT INTO student_tag (student_id, tag_id)
+VALUES (1, 11);
 -- Exo 2.4
 -- Mettez à jour le tag `HTML` :
 -- description : Le HTML est un language de structuration de données.
-
+UPDATE tag
+SET description = 'Le HTML est un language de structuration de données'
+WHERE id = 1
 -- Exo 2.5
 -- Mettez à jour le tag `Bash` :
 -- nom : Terminal Bash
 -- description : Le terminal Bash est l'interface en ligne de commande par défaut sous Linux.
-
+SELECT id FROM tag WHERE name = 'bash'; --pour retrouver l'id de Bash
+UPDATE tag
+SET name = 'Terminal Bash' description = 'Le terminal Bash est l interface en ligne de commande par défaut sous linux'.
+WHERE id = 10;
 -- Exo 2.6
 -- Supprimez la relation entre un student et un tag :
 -- - student : Zacharie Evrard
 -- - tag : Terminal Bash
-
+SELECT id FROM student WHERE firstname = 'Zacharie' AND lastname = 'Evrard' --id = 100
+SELECT id FROM tag WHERE name = 'Terminal Bash'; -- id = 10
+DELETE FROM student_tag WHERE student_id = 100 AND tag_id = 10;
 -- Exo 2.7
 -- Supprimez la relation entre un student et un project :
 -- - student : Arthur Lacombe
 -- - project : Dicta quia at qui
-
+SELECT id FROM project WHERE name = 'Dicta quia at qui.' -- l'id est 6
+SELECT id FROM student WHERE firstname = 'Arthur' AND lastname = 'Lacombe'; -- l'id est 16
+DELETE FROM student WHERE id = 16 AND project_id = 6;
 -- Exo 2.8
 -- Supprimez toutes les relations entre un student et ses tags :
 -- - student : Odette Thomas
-
+SELECT id FROM student WHERE firstname = 'Odette' AND lastname = 'Thomas' -- l'id est 99
+DELETE FROM student_tag WHERE student_id = 99;
 -- Exo 2.9
 -- Supprimez un student :
 -- - student : Odette Thomas
-
+DELETE FROM student WHERE id = 99;
 -- Exo 2.10
 -- Supprimez un project et toutes les relations avec ses dépendances, c-à-d les relations avec des student et les relations avec des tags :
 -- - project : Ut delectus explicabo.
 -- Vous devez écrire trois requêtes pour réaliser la tâche.
 -- Note : vous devez supprimez les relations avec les dépendances mais pas les dépendances.
 -- Autrement dit ne supprimez aucun student ni aucun tag.
-
+SELECT id FROM project WHERE name = 'Ut delectus explicabo.'; -- id est 16
+DELETE FROM project_tag WHERE project_id = 16;
+DELETE FROM student WHERE project_id = 16;
+DELETE FROM project WHERE id = 16;
